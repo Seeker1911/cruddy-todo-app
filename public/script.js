34517f7c53a45ecb9f6b7319fb773d1c784f789d
Scott Humphries <git@sscotth.io>
2016-05-27 09:35:50 -0500

    enable authenticated REST calls

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 $(() => {
   const API_URL = 'https://cruddy-todo-app.firebaseio.com/task'
+  let token = null
 
   const addItemToTable = (item, id) => {
     const row = `<tr data-id="${id}">
@@ -13,19 +14,20 @@ $(() => {
     $('tbody').append(row)
   }
 
-  // READ: GET data from firebase and display in table
-  $.get(`${API_URL}.json`)
-    .done((data) => {
-      if (data) {
-        // for (id in data) {
-        //   addItemToTable(data[id])
-        // }
+  const getTasks = () => {
+    $.get(`${API_URL}.json?auth=${token}`)
+      .done((data) => {
+        if (data) {
+          // for (id in data) {
+          //   addItemToTable(data[id])
+          // }
 
-        Object.keys(data).forEach((id) => {
-          addItemToTable(data[id], id)
-        })
-      }
-    })
+          Object.keys(data).forEach((id) => {
+            addItemToTable(data[id], id)
+          })
+        }
+      })
+  }
 
   // CREATE: form submit event to POST data to firebase
   $('.new form').submit(() => {
@@ -34,7 +36,7 @@ $(() => {
     //   method: 'POST',
     //   data: JSON.stringify({ task: 'I was posted!' })
     // })
-    $.post(`${API_URL}.json`,
+    $.post(`${API_URL}.json?auth=${token}`,
       JSON.stringify({ task: 'I was posted!' })
     )
     // TODO: Grab the form text
@@ -46,7 +48,7 @@ $(() => {
     const id = row.data('id')
 
     $.ajax({
-      url: `${API_URL}/${id}.json`,
+      url: `${API_URL}/${id}.json?auth=${token}`,
       method: 'DELETE'
     }).done(() => {
       row.remove()
@@ -58,6 +60,56 @@ $(() => {
     authDomain: "cruddy-todo-app.firebaseapp.com",
     databaseURL: "https://cruddy-todo-app.firebaseio.com",
     storageBucket: "cruddy-todo-app.appspot.com",
+  })
+
+  // both return promise like objects
+  const login = (email, password) => (
+    firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+  )
+
+  const register = (user, password) => (
+    firebase.auth().createUserWithEmailAndPassword(user, password)
+  )
+
+  $('.login form').submit((e) => {
+    const form = $(e.target)
+    const email = form.find('input[type="text"]').val()
+    const password = form.find('input[type="password"]').val()
+
+    login(email, password)
+      .then(console.log)
+      .catch(console.err)
+
+    e.preventDefault()
+  })
+
+  $('input[value="Register"]').click((e) => {
+    const form = $(e.target).closest('form')
+    const email = form.find('input[type="text"]').val()
+    const password = form.find('input[type="password"]').val()
+
+    register(email, password)
+      .then(() => login(email, password))
+      .then(console.log)
+      .catch(console.err)
+
+    e.preventDefault()
+  })
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // logged in
+      $('.app').show()
+
+      user.getToken()
+        .then(t => token = t)
+        .then(getTasks)
+
+    } else {
+      // logged out
+      $('.login').show()
+    }
   })
 
 })
